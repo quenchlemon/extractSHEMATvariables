7f309d24acb149f124a7b7a0050d880a7251dfd5
quenchlemon <jonathanpoh87@gmail.com>
2016-02-05 16:59:11 +0800

    Fixed bug by replacing version1 file with version2 contents

--- extractSHEMATaccterms.sh
+++ extractSHEMATaccterms.sh
@@ -5,15 +5,18 @@
 
 echo 'Extracting Courant values to text file'
 grep -A 3 'Courant-Number in x,y,z' $1 > courant.txt
-grep -A 2 'Max.' courant.txt > courant.txt
+grep -A 2 'Max.' courant.txt > courant2.txt
+mv courant2.txt courant.txt
 
 echo 'Extracting Neumann values to text file'
 grep -A 3 'Temperature-Neumann-Number in x,y,z' $1 > neumann.txt
-grep -A 2 'Max.' neumann.txt > neumann.txt
+grep -A 2 'Max.' neumann.txt > neumann2.txt
+mv neumann2.txt neumann.txt
 
 echo 'Extracting Peclet values to text file'
 grep -A 3 'Temperature-Peclet-Number in x,y,z' $1 > peclet.txt
-grep -A 2 'Max.' peclet.txt > peclet.txt
+grep -A 2 'Max.' peclet.txt > peclet2.txt
+mv peclet2.txt peclet.txt #removes version 2 file
 
 echo 'Done'
 
