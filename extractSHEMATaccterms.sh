28d563aff8f39bbca229c7c81b2b07410ec83a30
quenchlemon <jonathanpoh87@gmail.com>
2016-02-05 16:52:56 +0800

    Added line to cut off the first line

--- extractSHEMATaccterms.sh
+++ extractSHEMATaccterms.sh
@@ -5,12 +5,15 @@
 
 echo 'Extracting Courant values to text file'
 grep -A 3 'Courant-Number in x,y,z' $1 > courant.txt
+grep -A 2 'Max.' courant.txt > courant.txt
 
 echo 'Extracting Neumann values to text file'
 grep -A 3 'Temperature-Neumann-Number in x,y,z' $1 > neumann.txt
+grep -A 2 'Max.' neumann.txt > neumann.txt
 
 echo 'Extracting Peclet values to text file'
 grep -A 3 'Temperature-Peclet-Number in x,y,z' $1 > peclet.txt
+grep -A 2 'Max.' peclet.txt > peclet.txt
 
 echo 'Done'
 
